c9998378b76a8291a3f63082e1afc39b2cb05f6b
khabanegueye24 <khabanegueye020@gmail.com>
2026-02-12 15:49:50 +0100

    Add files via upload

---
+++ APP.py
@@ -0,0 +1,7 @@
+#Écrire un programme qui saisit deux notes d’un étudiant calcul et affiche la moyenne des notes.
+
+Not1 = int(input("sasir note1 :"))
+Not2 = int(input("sasir note2 :"))
+moyenne = note1 + note2
+
+print(" la moyenne est de",moyenne)
