1145f06e2a01ad36e5c200607c522e23b768a405
khabanegueye24 <khabanegueye020@gmail.com>
2026-02-12 16:11:59 +0100

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,10 +1,11 @@
 #Écrire un programme qui saisit deux notes d’un étudiant calcul et affiche la moyenne des notes.
 
 import streamlit as st 
-num1 =  st.input_number(" num1 : ")
-num2 =  st.input_number(" num2 : ")
+num1 =  st.number_input(" num1 : ")
+num2 =  st.number_input(" num2 : ")
 somme = num1 + num2
 
 if st.button("résultat"):
   st.info( somme )
   
+
